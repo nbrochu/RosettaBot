081f77533f83ad7bd510ae2450caae66ffd50190
Nicholas Brochu <info@nicholasbrochu.com>
2015-11-28 21:55:15 -0500

    added support for a custom command YAML file

--- lib/commands.py
+++ lib/commands.py
@@ -1,4 +1,10 @@
 import yaml
 
 with open("config/commands.yaml", "r") as f:
-    commands = yaml.load(f)
+    commands_default = yaml.load(f)
+
+with open("config/commands.custom.yaml", "r") as f:
+    commands_custom = yaml.load(f) or dict()
+
+commands = commands_default.copy()
+commands.update(commands_custom)
